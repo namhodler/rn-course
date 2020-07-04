f78bdae441545c33850de7d178ecb053e6851962
Elvis Kaito <namtran4194@gmail.com>
2020-07-04 14:59:25 +0700

    use props for ImageScreen

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -1,8 +1,17 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 
-const ImageDetail = () => {
-  return <Text>Image Screen</Text>;
+const ImageDetail = (props) => {
+  console.log(props)
+  return (
+    <View>
+      <Image source={props.img} />
+      <Text>{props.title}</Text>
+      <Text>Image score - {props.score}</Text>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({});
 
 export default ImageDetail;
--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -5,7 +5,21 @@ import ImageDetail from "../components/ImageDetail";
 const ImageScreen = () => {
   return (
     <View>
-      <ImageScreen />
+      <ImageDetail
+        title="Forest"
+        img={require("../../assets/forest.jpg")}
+        score={9}
+      />
+      <ImageDetail
+        title="Beach"
+        img={require("../../assets/beach.jpg")}
+        score={5}
+      />
+      <ImageDetail
+        title="Mountain"
+        img={require("../../assets/mountain.jpg")}
+        score={7}
+      />
     </View>
   );
 };
