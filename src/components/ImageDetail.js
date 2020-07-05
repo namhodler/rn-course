6dc93009f6e0c2feb7d41d99ab8b18c0df742e01
Elvis Kaito <namtran4194@gmail.com>
2020-07-05 23:33:07 +0700

    update

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -2,7 +2,6 @@ import React from "react";
 import { View, Text, StyleSheet, Image } from "react-native";
 
 const ImageDetail = (props) => {
-  console.log(props)
   return (
     <View>
       <Image source={props.img} />
